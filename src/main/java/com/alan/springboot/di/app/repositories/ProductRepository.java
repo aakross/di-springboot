6c8bb7a3efa4b4d4c868a205fbe853906e749fb7
aakross <royal9619@gmail.com>
2026-04-30 20:12:26 -0600

    first commit

---
+++ src/main/java/com/alan/springboot/di/app/repositories/ProductRepository.java
@@ -0,0 +1,24 @@
+package com.alan.springboot.di.app.repositories;
+
+import com.alan.springboot.di.app.models.Product;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ProductRepository {
+    private List<Product> data;
+
+    public ProductRepository(List<Product> data) {
+        this.data = Arrays.asList(
+                new Product(1L, "Memoria Corsair 2", 300L),
+                new Product(2L, "Cpu Intel core i9", 850L),
+                new Product(3L, "Teclado Razer Mini 60%", 80L),
+                new Product(4L, "Motherboard Gigabyte", 490L)
+        );
+    }
+
+    public List<Product> findAll() {
+        return data;
+    }
+
+}
