f8b71f585a9b67238fb04832fdb09dda54b3a98e
royal9619@gmail.com <naknin-byvpyg-0pIcra>
2026-05-16 14:59:06 -0600

    Como leer un json

--- src/main/java/com/alan/springboot/di/app/repositories/ProductRepositoryJson.java
+++ src/main/java/com/alan/springboot/di/app/repositories/ProductRepositoryJson.java
@@ -1,4 +1,44 @@
 package com.alan.springboot.di.app.repositories;
 
-public class ProductRepositoryJson {
+import com.alan.springboot.di.app.models.Product;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import tools.jackson.core.exc.StreamReadException;
+import tools.jackson.databind.DatabindException;
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class ProductRepositoryJson implements ProductRepository {
+
+    private List<Product> list;
+
+    public ProductRepositoryJson() {
+        Resource resource = new ClassPathResource("json/product.json");
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            list = Arrays.asList(objectMapper.readValue(resource.getFile(), Product[].class));
+        } catch (StreamReadException e) {
+            e.printStackTrace();
+        } catch (DatabindException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return null;
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return list.stream().filter(p -> {
+            return p.getId().equals(id);
+        }).findFirst().orElseThrow();
+    }
 }
